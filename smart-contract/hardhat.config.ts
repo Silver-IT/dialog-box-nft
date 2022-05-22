5b4bbb2fdf74723e3deedfe194e678f5c2f9297c
silver-it <alex.jin220@gmail.com>
2022-05-22 10:32:38 +0800

    feat: added rinkeby network and set it as default

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -24,10 +24,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
-  defaultNetwork: 'ropsten',
+  defaultNetwork: 'rinkeby',
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
