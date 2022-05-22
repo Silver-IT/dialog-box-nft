a57a869d22ff64bb96eaa1ecc8df4dfae8f21ee7
silver-it <alex.jin220@gmail.com>
2022-05-22 10:46:22 +0800

    feat: deploy ArtToken as default

--- smart-contract/scripts/deploy.ts
+++ smart-contract/scripts/deploy.ts
@@ -1,18 +1,34 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [deployer] = await ethers.getSigners()
+  const [deployer] = await ethers.getSigners();
   if (deployer === undefined) {
-      throw new Error('Deployer is undefined.')
+    throw new Error("Deployer is undefined.");
   }
-  console.log('Deploying contracts with the account:', deployer.address)
-  console.log('Account balance:', (await deployer.getBalance()).toString())
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // We get the contract to deploy
-  const ArtTokenManager = await ethers.getContractFactory("ArtTokenManager");
-  const artTokenManager = await ArtTokenManager.deploy();
+  // Deploy Art Token Contract
+  const ArtTokenFactory = await ethers.getContractFactory("ArtToken");
+  const artToken = await ArtTokenFactory.deploy(
+    "0x97Dee6068fDfD33e82385024B43018b476caD6F4",
+    "Art Token",
+    "ARTK",
+    "https://ipfs.com/baseurl/",
+    "https://ipfs.com/logo-url/"
+  );
+  await artToken.deployed();
+  console.log("ArtToken deployed to:", artToken.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // Deploy Art Token Manager Contract
+  const ArtTokenManagerFactory = await ethers.getContractFactory(
+    "ArtTokenManager"
+  );
+  const artTokenManager = await ArtTokenManagerFactory.deploy();
   await artTokenManager.deployed();
   console.log("ArtTokenManager deployed to:", artTokenManager.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 }
 
 main()
