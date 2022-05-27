327db8dc8ef16e5ab5ac41209de835d886454e0d
silver-it <alex.jin220@gmail.com>
2022-05-27 12:12:43 +0800

    feat: removed unnecessary contract imports

--- smart-contract/contracts/ArtTokenManager.sol
+++ smart-contract/contracts/ArtTokenManager.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "./ArtToken.sol";
 
 error InvalidCollectionAddress();
